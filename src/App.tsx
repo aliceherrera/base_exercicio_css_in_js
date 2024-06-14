8dd81762f4a3d140e9f5e6242af45e047fb40cdb
Alice Herrera <142321755+aliceherrera@users.noreply.github.com>
2024-06-14 14:37:49 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
+import globalStyle from './globalStyles'
 
 import './global.css'
 
 function App() {
   return (
     <>
+      <globalStyle />
       <Header />
       <Hero />
       <div className="container">
